69ba602c6a0ed5a2631491534c676d1f501fb795
Madhanmyil <47316868+Madhanmyil@users.noreply.github.com>
2020-07-28 11:44:26 +0530

    Add files via upload

---
+++ cha.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class cha {
+
+    public static void main(String[] args) {
+
+        char i;
+       
+        for( i= 'A'; i <= 'Z'; i++)
+            System.out.print(i + " ");
+    }
+}
