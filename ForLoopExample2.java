35355113423dd6181d7a939f0624cf48cf1c0345
Madhanmyil <47316868+Madhanmyil@users.noreply.github.com>
2020-07-30 11:29:41 +0530

    Add files via upload

---
+++ ForLoopExample2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+
+class ForLoopExample2{
+    public static void main(String args[]){
+         int arr[]={2,11,45,9};
+
+         for(int i=0; i<arr.length; i++){
+              System.out.println(arr[i]);
+         }
+    }
+}
